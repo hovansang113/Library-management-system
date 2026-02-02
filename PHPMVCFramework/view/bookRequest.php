658fb10eb3f8acdbfca41cc06416473804d1f97d
NhutNguyen25 <nguyentiennhut25102006@gmail.com>
2026-02-03 00:55:15 +0700

    feature-LIB-41-BookRequest

--- PHPMVCFramework/view/bookRequest.php
+++ PHPMVCFramework/view/bookRequest.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Book Request</title>
+    <link href="css/bookRequest.css" rel="stylesheet">
 </head>
 <body>
         <div class="card">
